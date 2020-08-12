eda8002b037a1a607020073deef0cbb20ecefc96
Tim Nash <tim.nash@34sp.com>
2020-08-12 20:05:05 +0100

    Initial Commit of Plugin

---
+++ README.txt
@@ -0,0 +1,3 @@
+Simple Utility Plugin
+
+Activate as either plugin or MU Plugin will then automatically turn on auto updates for plugins when they are activated turning WordPress Auto Updaters opt out not opt-in
---
+++ auto-activate-auto-updates.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Auto Activate Auto Updates
+Description: Switch to opt-out rather then opt-in for updates
+Version: 1.0.0
+Author: Tim Nash
+Author URI: https://timnash.co.uk
+*/
+
+/**
+ *  Triggers on any activation of plugin
+ *
+ * @since 1.0.0
+ * @return bool
+ */
+add_action( 'activated_plugin', function($plugin, $network){
+	$auto_updates   = (array) get_site_option( 'auto_update_plugins', array() );
+	$auto_updates[] = $plugin;
+	$auto_updates   = array_unique( $auto_updates );
+
+	$all_items      = apply_filters( 'all_plugins', get_plugins() );
+	$auto_updates   = array_intersect( $auto_updates, array_keys( $all_items ) );
+
+	// Update or set the auto update option
+	update_site_option( 'auto_update_plugins', $auto_updates );
+ }, 10,2);
