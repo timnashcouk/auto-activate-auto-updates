6d5241fcf157d5cb3d0e8b33163cbea041389616
Tim Nash <tim.nash@34sp.com>
2020-08-20 20:09:10 +0100

    [FEATURE] Auto Update existing plugins - Auto update existing plugins, on plugin activation if nothing is in the array to start fixes #2

--- README.txt
+++ README.txt
@@ -1,3 +1,8 @@
 Simple Utility Plugin
 
 Activate as either plugin or MU Plugin will then automatically turn on auto updates for plugins when they are activated turning WordPress Auto Updaters opt out not opt-in
+
+#Change Log
+
+v1.1.0 - Added Auto Updating for preexisting plugins, if none have already been added to the auto update system.
+V1.0 - Initial release
--- auto-activate-auto-updates.php
+++ auto-activate-auto-updates.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Auto Activate Auto Updates
 Description: Switch to opt-out rather then opt-in for updates
-Version: 1.0.0
+Version: 1.1.0
 Author: Tim Nash
 Author URI: https://timnash.co.uk
 */
@@ -24,3 +24,17 @@ add_action( 'activated_plugin', function($plugin, $network){
 	// Update or set the auto update option
 	update_site_option( 'auto_update_plugins', $auto_updates );
  }, 10,2);
+
+/**
+ * Triggers Activation Hook when plugin is first activated
+ * @since 1.1.0
+ * @return bool
+ */
+register_activation_hook( __FILE__, function(){
+	$auto_updates   = (array) get_site_option( 'auto_update_plugins', array() );
+	if( !empty( $auto_updates ) ){
+		$all_items    = apply_filters( 'all_plugins', get_plugins() );
+		return update_site_option( 'auto_update_plugins', array_keys( $all_items ) );
+	}
+	return true;
+});
